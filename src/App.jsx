c5ada08a93ff5bfc1475f6557e512bc8dfaec7f5
mogunecire <emsi.ilija@gmail.com>
2025-08-07 13:54:50 +0200

    back to default

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <main>
       <section className="container">
-        <h3>{people.length} birthdays yesterday</h3>
+        <h3>{people.length} birthdays today</h3>
         <List people={people}></List>
         <button type="button" className="btn btn-block" onClick={clearItems}>
           clear all
